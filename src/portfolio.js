b6f7515c40abeac00f5296f07607eb839445000b
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-12-24 00:33:23 +0800

    added blog

--- src/portfolio.js
+++ src/portfolio.js
@@ -188,15 +188,8 @@ const blogSection = {
   subtitle:
     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
-  blogs: [
-    {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    }
-  ],
-  display: false // Set false to hide this section, defaults to true
+  blogs: [],
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
